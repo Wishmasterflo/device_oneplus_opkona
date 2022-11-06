c1744f16ab8af88eb5ac68abe25aa60ff30f232d
Wishmasterflo <flo@wishmasterflo.de>
2022-11-06 13:12:49 +0100

    Updated unified-script for Oneplus 8 and 8 Pro

--- recovery/root/system/bin/unified-script.sh
+++ recovery/root/system/bin/unified-script.sh
@@ -2,6 +2,30 @@
 # automatically set device props for unified tree shared-hardware models/variants
 
 
+load_op8()
+{
+    resetprop "ro.build.product" "OnePlus8"
+    resetprop "ro.display.series" "OnePlus 8"
+    resetprop "ro.product.device" "OnePlus8"
+    resetprop "ro.product.model" "OnePlus 8"
+    resetprop "ro.product.name" "OnePlus8"
+    resetprop "ro.product.odm.device" "OnePlus8"
+    resetprop "ro.product.odm.model" "OnePlus 8"
+    resetprop "ro.product.odm.name" "OnePlus8"
+    resetprop "ro.product.product.device" "OnePlus8"
+    resetprop "ro.product.product.model" "OnePlus 8"
+    resetprop "ro.product.product.name" "OnePlus8"
+    resetprop "ro.product.system.device" "OnePlus8"
+    resetprop "ro.product.system.model" "OnePlus 8"
+    resetprop "ro.product.system.name" "OnePlus8"
+    resetprop "ro.product.system_ext.device" "OnePlus8"
+    resetprop "ro.product.system_ext.model" "OnePlus 8"
+    resetprop "ro.product.system_ext.name" "OnePlus8"
+    resetprop "ro.product.vendor.device" "OnePlus8"
+    resetprop "ro.product.vendor.model" "OnePlus 8"
+    resetprop "ro.product.vendor.name" "OnePlus8"
+}
+
 load_op8t()
 {
     resetprop "ro.build.product" "OnePlus8T"
@@ -24,6 +48,30 @@ load_op8t()
     resetprop "ro.product.vendor.device" "OnePlus8T"
     resetprop "ro.product.vendor.model" "OnePlus 8T"
     resetprop "ro.product.vendor.name" "OnePlus8T"
+}
+
+load_op8pro()
+{
+    resetprop "ro.build.product" "OnePlus8Pro"
+    resetprop "ro.display.series" "OnePlus 8Pro"
+    resetprop "ro.product.device" "OnePlus8PRO"
+    resetprop "ro.product.model" "OnePlus 8 Pro"
+    resetprop "ro.product.name" "OnePlus8Pro_IND"
+    resetprop "ro.product.odm.device" "OnePlus8Pro"
+    resetprop "ro.product.odm.model" "OnePlus 8Pro"
+    resetprop "ro.product.odm.name" "OnePlus8Pro_IND"
+    resetprop "ro.product.product.device" "OnePlus8Pro"
+    resetprop "ro.product.product.model" "OnePlus 8Pro"
+    resetprop "ro.product.product.name" "OnePlus8Pro_IND"
+    resetprop "ro.product.system.device" "OnePlus8Pro"
+    resetprop "ro.product.system.model" "OnePlus 8Pro"
+    resetprop "ro.product.system.name" "OnePlus8Pro_IND"
+    resetprop "ro.product.system_ext.device" "OnePlus8Pro"
+    resetprop "ro.product.system_ext.model" "OnePlus 8Pro"
+    resetprop "ro.product.system_ext.name" "OnePlus8Pro_IND"
+    resetprop "ro.product.vendor.device" "OnePlus8Pro"
+    resetprop "ro.product.vendor.model" "OnePlus 8Pro"
+    resetprop "ro.product.vendor.name" "OnePlus8Pro_IND"
 }
 
 load_op9r()
@@ -57,6 +105,12 @@ case $project in
     20828)
         load_op9r
         ;;
+	12)
+	    load_op8
+		;;
+	19811)
+        load_op8pro
+        ;;		
     *)
         load_op8t
         ;;

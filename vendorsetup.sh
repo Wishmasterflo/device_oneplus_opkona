76ad62472826dba675542a7a972c3168a4494558
Wishmasterflo <72379104+Wishmasterflo@users.noreply.github.com>
2023-01-09 00:37:50 +0100

    Cleanup file
    Cleanup file

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ export OF_MAINTAINER=Wishmasterflo
 # Build environment stuffs
 export FOX_BUILD_DEVICE="OnePlus8T"
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
 export TARGET_DEVICE_ALT="kebab, Kebab, instantnoodle, instantnoodlep, lemonades, lemonkebab, OnePlus8, OnePlus 8, OnePlus8t, OnePlus 8t, OnePlus 8 Pro"
 export OF_TARGET_DEVICES="kebab, Kebab, instantnoodle, instantnoodlep, lemonades, lemonkebab, OnePlus8, OnePlus 8, OnePlus8t, OnePlus 8t, OnePlus 8 Pro"
@@ -19,9 +19,9 @@ export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
 # We have a/b partitions
-export OF_AB_DEVICE="1"
-export OF_VIRTUAL_AB_DEVICE="1"
-export OF_AB_DEVICE_WITH_RECOVERY_PARTITION="1"
+export OF_AB_DEVICE=1
+export OF_VIRTUAL_AB_DEVICE=1
+export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
 
 # Screen specifications
 #export OF_SCREEN_H=1920
@@ -33,8 +33,8 @@ export OF_ALLOW_DISABLE_NAVBAR=0
 export OF_CLOCK_POS=0
 
 # Device stuff
-export OF_KEEP_FORCED_ENCRYPTION="1"
-export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+export OF_KEEP_FORCED_ENCRYPTION=1
+export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 export OF_FBE_METADATA_MOUNT_IGNORE=1
 export OF_PATCH_AVB20=1
 
@@ -60,15 +60,15 @@ export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
 export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 
 # -- add settings for Android 12 --
-# export OF_FIX_DECRYPTION_ON_DATA_MEDIA="1"
+# export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
 
 # Add some extras
-export FOX_USE_ZIP_BINARY="1"
-export FOX_USE_TAR_BINARY="1"
-export FOX_ASH_IS_BASH="1"
-export FOX_REPLACE_BUSYBOX_PS="1"
-export FOX_USE_BASH_SHELL="1"
-export FOX_USE_LZMA_COMPRESSION="1"
-export FOX_USE_NANO_EDITOR="1"
-export OF_DONT_KEEP_LOG_HISTORY="1"
-export OF_NO_SPLASH_CHANGE="1"
+export FOX_USE_ZIP_BINARY=1
+export FOX_USE_TAR_BINARY=1
+export FOX_ASH_IS_BASH=1
+export FOX_REPLACE_BUSYBOX_PS=1
+export FOX_USE_BASH_SHELL=1
+export FOX_USE_LZMA_COMPRESSION=1
+export FOX_USE_NANO_EDITOR=1
+export OF_DONT_KEEP_LOG_HISTORY=1
+export OF_NO_SPLASH_CHANGE=1

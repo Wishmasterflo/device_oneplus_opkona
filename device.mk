914244677d29b4297e093e65cbf9d57933f939b2
Wishmasterflo <72379104+Wishmasterflo@users.noreply.github.com>
2023-04-01 00:09:46 +0200

    Changed to Fastboot 1.1
    Changed to Fastboot 1.1

--- device.mk
+++ device.mk
@@ -86,7 +86,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock \
+    android.hardware.fastboot@1.1-impl-mock.recovery \
     fastbootd \
     resetprop
 

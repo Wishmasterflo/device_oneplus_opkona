2350c39564340236201e2bc3e78221539cec91c7
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2023-10-01 00:32:39 +0200

    Magisk-v26.3
    Added Magisk Addon Magisk-v26.3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -51,6 +51,9 @@ export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
 # Qick Backup list
 export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 
+# Magisk 26.3
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.3.zip
+
 # Add some extras
 export FOX_USE_ZIP_BINARY=1
 export FOX_USE_TAR_BINARY=1

6dc103920cf0339a0fa1cfd466a805dca1253c09
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2023-10-16 16:41:31 +0200

    Dont install AROMAFM
    Dont install AROMAFM

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -54,6 +54,9 @@ export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 # Magisk 26.3
 export FOX_USE_SPECIFIC_MAGISK_ZIP=device/oneplus/opkona/Magisk/Magisk-v26.3.zip
 
+# Dont install AROMAFM
+export FOX_DELETE_AROMAFM=1
+
 # Add some extras
 export FOX_USE_ZIP_BINARY=1
 export FOX_USE_TAR_BINARY=1

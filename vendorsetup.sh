bc6aeafa5e917daf6f1fbad461379d8529f6dbec
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2023-11-09 18:35:39 +0100

    Updated to Magisk 26.4
    Updated to Magisk 26.4

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,7 +57,7 @@ export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
 # Magisk 26.3
-export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.3.zip
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.4.zip
 
 # Dont install AROMAFM
 export FOX_DELETE_AROMAFM=1

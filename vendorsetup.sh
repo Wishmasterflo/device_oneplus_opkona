e3b88448d2d58f8d94711baf3dac670298b98438
Flo <72379104+Wishmasterflo@users.noreply.github.com>
2023-12-17 15:49:15 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -57,7 +57,7 @@ export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
 # Set default Keymaster Version
 export OF_DEFAULT_KEYMASTER_VERSION=4.1
 
-# Magisk 26.3
+# Magisk
 export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.4.zip
 
 # Dont install AROMAFM
